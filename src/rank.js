2286e2c21a3032a84dec60d34006d91c66f3a5d1
benjamin <529709737@qq.com>
2020-08-29 03:56:06 +0800

    [refactor][Benjamin] refactor: add method to class ExperiencedChinaRating

--- src/rank.js
+++ src/rank.js
@@ -83,6 +83,25 @@ class ExperiencedChinaRating extends Rating{
       const result = super.captainHistoryRisk - 2;
       return Math.max(result, 0);
   }
+
+  get voyageProfitFactor() {
+    return super.voyageProfitFactor + 3;
+}
+
+  get voyageLengthFactor() {
+      let result = 0;
+      if (this.voyage.length > 12) {
+          result += 1;
+      }
+      if (this.voyage.length > 18) {
+          result -= 1;
+      }
+      return result;
+  }
+
+  get historyLengthFactor() {
+      return (this.history.length > 10) ? 1 : 0;
+  }
 }
 
 function createRating(voyage, history) {
